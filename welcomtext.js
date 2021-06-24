8ed205ed479a4ca7bf3427651216465be5566fcf
Kalilinuxhcbr <80177940+Kalilinuxhcbr@users.noreply.github.com>
2021-06-24 13:58:19 -0300

    firts commit

---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adeus @${number}. 𝐌𝐞𝐭𝐞 𝐨 𝐩𝐞 𝐬𝐞𝐮 𝐩𝐨𝐫𝐫𝐚! 👋`
+}
+exports.bye = bye
